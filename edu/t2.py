bb1f681b2f15192a7bfa78b177f2403c3d17dc6e
Ivens <ivensportugal@me.com>
2024-08-03 08:58:04 -0400

    change

--- edu/t2.py
+++ edu/t2.py
@@ -1,35 +1,52 @@
 import torch
 from torch import nn
+from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transforms import ToTensor
 
-# Download training data from open datasets
-training_data = datasets.FashionMNIST(
-	root="data",
-	train=True,
-	download=True,
-	transform=ToTensor(),
-)
+import pandas as pd
+import numpy as np
 
-# Download test data from open datasets.
-test_data = datasets.FashionMNIST(
-	root='data',
-	train=False,
-	download= True,
-	transform=ToTensor(),
-)
+# Dataset
 
-batch_size = 64
+class CSVDataset(Dataset):
 
-# Create data loaders.
-train_dataloader = DataLoader(training_data, batch_size=batch_size)
-test_dataloader = DataLoader(test_data, batch_size=batch_size)
+	def __init__(self, filename):
+		df = pd.read_csv(filename)
+		self.n = df.shape[0]
 
-for X, y in test_dataloader:
-	print(f'Shape of X [N, C, H, W]: {X.shape}')
-	print(f'Shape of y: {y.shape} {y.dtype}')
-	break
+		Xcol = []
+		Ycol = []
+
+		for col in df.columns:
+			if 'x' in col: Xcol.append(col)
+			else: Ycol.append(col)
+
+		self.X = df[Xcol].to_numpy()
+		self.Y = df[Ycol].to_numpy()
+
+	def __len__(self):
+		return self.n
+
+	def __getitem__(self, idx):
+		if torch.is_tensor(idx):
+			idk = idk.to_list()
+
+		return torch.tensor(self.X[idx,:], dtype=torch.float), torch.tensor(self.Y[idx,:], dtype=torch.float)
+
+
+
+training_data = CSVDataset('input.csv')
+
+
+
+# Data loader
+
+train_dataloader = DataLoader(training_data, batch_size=64)
+test_dataloader = DataLoader(training_data, batch_size=64)
+
+# for X, y in train_dataloader:
+# 	print(X)
+# 	print(y)
 
 # Get cpu, gpu or mps device for training.
 device = (
@@ -39,33 +56,30 @@ device = (
 	if torch.backends.mps.is_available()
 	else 'cpu'
 )
-print(f'Using {device} device')
+
+
 
 # Define model
 class NeuralNetwork(nn.Module):
 	def __init__(self):
 		super().__init__()
 		self.flatten = nn.Flatten()
-		self.linear_relu_stack = nn.Sequential(
-			nn.Linear(28*28, 512),
-			nn.ReLU(),
-			nn.Linear(512,512),
-			nn.ReLU(),
-			nn.Linear(512, 10)
+		self.linear_stack = nn.Sequential(
+			nn.Linear(2, 1),
+			# nn.ReLU()
 		)
 
 	def forward(self, x):
 		x = self.flatten(x)
-		logits = self.linear_relu_stack(x)
+		logits = self.linear_stack(x)
 		return logits
 
 model = NeuralNetwork().to(device)
 print(model)
 
 
-
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+loss_fn = nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
 
 def train(dataloader, model, loss_fn, optimizer):
 	size = len(dataloader.dataset)
@@ -76,6 +90,11 @@ def train(dataloader, model, loss_fn, optimizer):
 		# Compute prediction error
 		pred = model(X)
 		loss = loss_fn(pred, y)
+		# print(f'pred: {pred}')
+		# print(f'y: {y}')
+		# print(f'loss: {loss}')
+		# print(f'-------')
+		# input()
 
 		# Backpropagation
 		loss.backward()
@@ -83,7 +102,7 @@ def train(dataloader, model, loss_fn, optimizer):
 		optimizer.zero_grad()
 
 		if batch % 100 == 0:
-			loss, current = loss.item(), (batch + 1) * len(X)
+			loss, current = loss.item(), (batch+1) * len(X)
 			print(f'loss: {loss:>7f} [{current:>5d}/{size:>5d}]')
 
 
@@ -100,13 +119,17 @@ def test(dataloader, model, loss_fn):
 			correct += (pred.argmax(1) == y).type(torch.float).sum().item()
 	test_loss /= num_batches
 	correct /= size
-	print(f'Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n')
+	print(f'Test Error: \n Accuracy: {(100*correct):>0.1f}% Avg loss: {test_loss:>8f} \n')
 
 
-epochs = 50
+
+epochs = 100
 for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
     train(train_dataloader, model, loss_fn, optimizer)
     test(test_dataloader, model, loss_fn)
 print("Done!")
 
+x = torch.tensor([[51,2]],dtype=torch.float).to(device)
+y = model(x)
+print(y.item())
